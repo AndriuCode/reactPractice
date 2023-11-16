24d37cd3b7c47284d0c0e68dedce7143e270ab9d
AndriuCode <vivanco.sebastian.a@gmail.com>
2023-11-15 21:42:45 -0500

    Build a component from Twitter (1) - Set basic styles and some data 🚧

---
+++ projects/firstProject/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export function App() {
+    return (
+        <article>
+            <header>
+                <img alt="Avatar de AndriuCode" src="https://unavatar.io/andriuCode" />
+                <div>
+                    <strong>Andriu Code V.</strong>
+                    <span>@andriuCode</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
